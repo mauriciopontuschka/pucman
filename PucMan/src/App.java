26ac99c62528a318d8962a1e2c7c1e5b6cd7c140
Maurício Nacib Pontuschka <mauricio@realide.com>
2014-02-19 21:23:59 -0300

    novo commit de teste

---
+++ PucMan/src/App.java
@@ -0,0 +1,26 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		Posicao p1 = new Posicao("A");
+		Posicao p2 = new Posicao("B");
+		Posicao p3 = new Posicao("C");
+		Posicao p4 = new Posicao("D");
+		
+		p1.addPath('d', p2);
+		p1.addPath('s', p3);
+		
+		p2.addPath('s', p4);
+		p2.addPath('a', p1);
+
+		p3.addPath('w', p1);
+		p3.addPath('d', p4);
+
+		p4.addPath('a', p3);
+		p4.addPath('w', p2);
+		
+		
+
+	}
+
+}
--- PucMan/src/Posicao.java
+++ PucMan/src/Posicao.java
@@ -1,4 +1,29 @@
+import java.util.Hashtable;
+
 
 public class Posicao {
-
+	
+	private String label;
+	private Hashtable<Character, Posicao> pathList;
+	
+	public Posicao(String label){
+		this.setLabel(label);
+	}
+	
+	public void addPath(char key, Posicao path){
+		pathList.put(key, path);
+	}
+	
+	public Posicao getPath(char key){
+		return pathList.get(key);
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	
 }
